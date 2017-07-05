d1a210794842fb02597e92c0278347f35c18445a
Root-C <hpalacios@disolu.com>
2017-07-05 15:08:16 -0500

    a

--- js/donation.js
+++ js/donation.js
@@ -10,16 +10,16 @@
                 '<label class="donation-block align-left donation-text"><b>Tipo de Moneda</b>'+
                 '</label>'+
                 '<div class="donation-currency">'+
-                    '<button class="donation-inactive donation-button-default donate-currency" style="'+
-            'margin-right: 10px;">USD</button>'+
+                    '<button class="donation-inactive donation-button-default donate-currency">USD</button>'+
                     '<button class="donation-inactive donation-button-default donate-currency">Soles</button>'+
                 '</div>'+
                 '<label class="donation-block align-left donation-text"><b>¿Cuánto deseas donar?</b>'+
                 '</label>'+
                 '<div class="amount">'+
-                    '<button class="button-45 donation-button-default donate-amount donation-inactive" style="'+
-    '   margin-right: 10px;">20</button>'+
+                    '<button class="button-45 donation-button-default donate-amount donation-inactive">20</button>'+
                     '<button class="button-45 donation-button-default donate-amount donation-inactive">50</button>'+
+                     '<button class="button-45 donation-button-default donate-amount donation-inactive">100</button>'+
+                    '<button class="button-45 donation-button-default donate-amount donation-inactive">500</button>'+
                 '</div>'+
                 '<label class="donation-block align-left donation-text">Otras cantidades</label>'+
                 '<input type="text" id="customamount" class="donation-input" placeholder="Ej: 10">'+
@@ -27,9 +27,9 @@
             '</div>'+
             '<div class="donation" style="">'+
                 '<div class="donation-block"><img class="donation-image" src="https://www.joinnus.com/inc/donation/images/img-latav2.png">'+
-                '</div><span class="donation-block donation-message">DONA AQUÍ</span><img class="powered" src="https://www.joinnus.com/inc/donation/images/powered.png" alt="">'+
+                '</div><span class="donation-block donation-message">DONA AQUÍ</span><img class="powered_joinnus" src="https://www.joinnus.com/inc/donation/images/powered.png" alt="">'+
             '</div>'+
-        '</div>';
+        '</div>'
 		document.body.appendChild(template);
 
 
@@ -57,26 +57,24 @@
     for (var i = 0; i< amountclass.length; i++) {
     	amountclass[i].onclick = function() {
     		amount = parseInt(this.innerText);
-            for(var j = 0; j<currencyclass.length; j++) {
+            for(var j = 0; j<amountclass.length; j++) {
                 amountclass[j].classList.remove('donation-active');
             }
             this.classList.add('donation-active');
     	}
     }
 
+    //  isNaN(textamount) == false <- es numero
 	var donate = document.getElementById('donate').onclick = function() {
 		var textamount = document.getElementById('customamount').value;
-		if(textamount != '') {
-			if(isNaN(textamount) == false) {
-				amount = textamount;
-			} else {
-				alert('Por favor elige un monto');
-				return false;
-			}
-		} 
-		if(amount == 0) {
-			alert('Por favor elige un monto para donar');
-			return false;
-		}
-		window.open('https://www.joinnus.com/donacion/fpc?amount='+ amount +'&currency=' + currency, '_blank');
+		if(amount != 0 && textamount == '') {
+			if(isNaN(amount) == false) {
+                window.open('https://www.joinnus.com/donacion/fpc?amount='+ amount +'&currency=' + currency, '_blank');
+			} 
+		} else if(amount == 0 && textamount=='' || textamount<21){
+                alert('Por favor elige un monto mayor a 20 para tu donación');
+                return false;
+            } else {
+                window.open('https://www.joinnus.com/donacion/fpc?amount='+ textamount +'&currency=' + currency, '_blank');
+        }
 	};
